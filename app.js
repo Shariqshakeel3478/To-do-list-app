693f9ea1aa4072ab3823614a6e0b9220e820df46
shahbaz <muhammad.shahbazt20@gmail.com>
2025-05-12 19:26:07 +0500

    create/read-feature

--- app.js
+++ app.js
@@ -3,3 +3,28 @@ document.getElementById('all').addEventListener('click', function () {
 })
 
 
+fetch('read.php')
+.then(response=>response.json())
+.then(data=>{
+    console.log(data)
+    let ul = document.getElementById('ul')
+    ul.innerHTML = ''
+    data.forEach(task => {
+        ul.innerHTML+= `<li class="task">
+      <input type="radio" id="task-1" />
+      <p>${task.tasks}</p>
+      <div class="due">
+        <i class="fa-solid fa-calendar"></i>
+        <p>${task.due_date}</p>
+      </div>
+      <div class="buttons">
+        <button><i class="fa-solid fa-pencil"></i></button>
+        <button><i class="fa-solid fa-xmark"></i></button>
+      </div>
+    </li>`;
+    });
+    
+
+    
+})
+
